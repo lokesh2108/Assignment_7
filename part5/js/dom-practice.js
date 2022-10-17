fe24d56804aac7e0cb16d937a4c0ee48fe6409ed
lokesh2108 <65254815+lokesh2108@users.noreply.github.com>
2022-10-16 23:06:56 -0700

    part 5

--- part5/js/dom-practice.js
+++ part5/js/dom-practice.js
@@ -1,12 +1,34 @@
 /*eslint-env browser*/
 
 //STEP 1
+function displayTheMessage() {
+    window.alert("I have been clicked");
+}
 
-//STEP 2
 
-//STEP 3
+function main() {
+    //STEP 2
+    var buttonElement_1 = document.getElementById("unique_Button_1");
+    buttonElement_1.onclick = function () {
+        window.alert("I have been clicked");
+    }
 
-//STEP 4
+    //STEP 3
+    var buttonElement_2 = document.getElementById("unique_Button_2");
+    function userFunction() {
+        window.alert("I have been clicked");
+    }
+    buttonElement_2.addEventListener("click", userFunction);
 
-//STEP 5
+    //STEP 4
+    var buttonElement_3 = document.getElementById("unique_Button_3");
+    buttonElement_3.addEventListener("click", function() {
+        window.alert("I have been clicked");
+    });
 
+    //STEP 5
+    buttonElement_3.addEventListener("click", function() {
+        window.alert("I have been clicked");
+    });
+}
+window.addEventListener("load", main);
